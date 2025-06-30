c5a9561501ba5708a5e1d732533655a0b7889889
Suhas4518 <annigerisuhas@gmail.com>
2025-06-30 11:15:42 +0530

    Delete Exercises directory

--- Exercises/Design Patterns and Principles/Exercise 4 Implementing the Adapter Pattern/Code/RazorpayGateway.java
+++
@@ -1,5 +0,0 @@
-public class RazorpayGateway {
-    public void makeRazorpayPayment(double amount) {
-        System.out.println("Payment of Rs " + amount + " processed via Razorpay.");
-    }
-}
--- Exercises/Design Patterns and Principles/Exercise 4 Implementing the Adapter Pattern/Code/StripeGateway.java
+++
@@ -1,5 +0,0 @@
-public class StripeGateway {
-    public void stripePay(double amountInUSD) {
-        System.out.println("Payment of $" + amountInUSD + " processed via Stripe.");
-    }
-}
